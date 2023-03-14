f9709d49733917613e853cfff79adf29fa0c8f87
Camille Gachido <camillegachido70@gmail.com>
2023-03-14 14:49:57 -0300

    feat: add sign-in router

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import SignIn from "../pages/sign-in";
+
+function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<SignIn />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes;
