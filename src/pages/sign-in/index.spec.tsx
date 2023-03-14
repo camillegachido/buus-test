b07a3f67a86e6ae9b0b4a2b89975e81a09d0a962
Camille Gachido <camillegachido70@gmail.com>
2023-03-14 20:07:44 -0300

    fix: change tests rules from sign-in page

--- src/pages/sign-in/index.spec.tsx
+++ src/pages/sign-in/index.spec.tsx
@@ -4,20 +4,53 @@ import SignIn from '.';
 import { options } from './options'
 
 describe('Page: Sign in', () => {
-  it.skip('should render form', async () => {
+  it('should render form', async () => {
     const { container } = render(<SignIn />);
     const form = container.querySelector('form');
 
     expect(form).toBeInTheDocument();
   });
-  it('should display error if fields are empty', async () => {
+  it('should start with button disabled', async () => {
     render(<SignIn />);
+
     const button = await screen.findByText(/Sign In/)
-    await user.click(button)
+    expect(button).toBeDisabled();
+  })
+  it('should disable button if username field is empty', async () => {
+    render(<SignIn />);
 
-    const errorsLabel = await screen.queryAllByText(/Empty field!/)
+    const usernameContainer = await screen.getByTestId(options.username)
+    const usernameInput = usernameContainer.querySelector(`#${options.username}`) as HTMLElement;
+    await user.type(usernameInput, 'Text')
+    await user.clear(usernameInput)
 
-    expect(errorsLabel.length).toBe(2)
+
+    const button = await screen.findByText(/Sign In/)
+    expect(button).toBeDisabled();
+  });
+  it('should disable button if password field is empty', async () => {
+    render(<SignIn />);
+
+    const passwordContainer = await screen.getByTestId(options.password)
+    const passwordInput = passwordContainer.querySelector(`#${options.password}`) as HTMLElement;
+    await user.type(passwordInput, 'Text')
+    await user.clear(passwordInput)
+
+    const button = await screen.findByText(/Sign In/)
+    expect(button).toBeDisabled();
+  });
+  it('should enable button if fields are filled', async () => {
+    render(<SignIn />);
+    const usernameContainer = await screen.getByTestId(options.username)
+    const usernameInput = usernameContainer.querySelector(`#${options.username}`) as HTMLElement;
+    await user.type(usernameInput, 'Text')
+
+    const passwordContainer = await screen.getByTestId(options.password)
+    const passwordInput = passwordContainer.querySelector(`#${options.password}`) as HTMLElement;
+    await user.type(passwordInput, 'Text')
+
+    const button = await screen.findByText(/Sign In/)
+    expect(button).toBeEnabled();
   });
   it('should change page if username and password are filled', async () => {
     render(<SignIn />);
