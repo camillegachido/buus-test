b983e4f636e1cdc33908db293c0443997debb8f8
Camille Gachido <camillegachido70@gmail.com>
2023-03-14 15:54:37 -0300

    feat: add error message to input component

--- src/components/input/index.spec.tsx
+++ src/components/input/index.spec.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event'
+
 import Input from './'
 
 describe('input', () => {
@@ -6,10 +8,28 @@ describe('input', () => {
     render(<Input
       id="id-test"
       name="name-test"
-      label="label-test" />);
+      label="label-test"
+
+      value=''
+      onChange={() => { }}
+    />);
 
     const component = await screen.getByTestId('input')
 
     expect(component).toBeTruthy()
-  })
+  });
+  it('should display error message if error is true', async () => {
+    render(<Input
+      id="id-test"
+      name="name-test"
+      label="label-test"
+
+      value=''
+      onChange={() => { }}
+      error={true}
+    />);
+
+    const component = await screen.queryByText(/Empty field!/)
+    expect(component).toBeInTheDocument()
+  });
 })
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -5,18 +5,26 @@ interface IProps {
   name: string;
   id: string;
   type?: string;
+  error?: boolean;
+
+  value: string;
+  onChange: () => void
 }
 
 function Input(props: IProps) {
   return (
-    <TextField
-      margin="normal"
-      required
-      fullWidth
-      autoFocus
-      data-testid="input"
-      {...props}
-    />
+    <>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        autoFocus
+        data-testid="input"
+        helperText={props.error ? 'Empty field!' : ''}
+        {...props}
+      />
+    </>
+
   )
 }
 
