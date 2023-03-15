0310dfbbc255e4bec375c6420373e3492af7e544
Camille Gachido <camillegachido70@gmail.com>
2023-03-15 00:43:49 -0300

    test: add apply button test at embarks-filters

--- src/pages/embarks/components/embarks-filters/index.spec.tsx
+++ src/pages/embarks/components/embarks-filters/index.spec.tsx
@@ -5,28 +5,37 @@ import { options } from '../../options';
 
 describe('Embarks filters', () => {
   it('should render filters', () => {
-    render(<EmbarksFilters />)
+    render(<EmbarksFilters onApply={() => { }} />)
 
     const filterContainer = screen.queryByTestId(options.filter)
     expect(filterContainer).toBeInTheDocument();
   });
   it('should not display extend filters by default', () => {
-    render(<EmbarksFilters />)
+    render(<EmbarksFilters onApply={() => { }} />)
 
     const filterContainer = screen.queryByTestId(options.expandedFilter)
     expect(filterContainer).not.toBeInTheDocument();
   });
   it('should display extend filters after click', async () => {
-    render(<EmbarksFilters />)
+    render(<EmbarksFilters onApply={() => { }} />)
 
-    const button = await screen.getByText(/More filters/)
+    const button = screen.getByText(/More filters/)
     await user.click(button)
 
     const filterContainer = screen.queryByTestId(options.expandedFilter)
     expect(filterContainer).toBeInTheDocument();
   });
+  it('should change button "More filters" text to "Hide filters"', async () => {
+    render(<EmbarksFilters onApply={() => { }} />)
+
+    const button = screen.getByText(/More filters/)
+    await user.click(button)
+
+    const changedButton = screen.getByText(/Hide filters/)
+    expect(changedButton).toBeInTheDocument();
+  });
   it('should hide extend filters after two clicks', async () => {
-    render(<EmbarksFilters />)
+    render(<EmbarksFilters onApply={() => { }} />)
 
     const button = await screen.getByText(/More filters/)
     await user.click(button)
@@ -34,7 +43,17 @@ describe('Embarks filters', () => {
     const filterContainer = screen.queryByTestId(options.expandedFilter)
     expect(filterContainer).toBeInTheDocument();
 
-    await user.click(button)
+    const changedButton = await screen.getByText(/Hide filters/)
+    await user.click(changedButton)
     expect(filterContainer).not.toBeInTheDocument();
+  });
+  it('should call function when click on "Apply" button', async () => {
+    const mock = jest.fn(() => { })
+    render(<EmbarksFilters onApply={mock} />)
+
+    const button = screen.getByText(/Apply/)
+    await user.click(button)
+
+    expect(mock.mock.calls).toHaveLength(1);
   });
 })
